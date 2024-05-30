bf0d50b9105403cca26d20ea041b571c0e993fdb
cesargalvan <cesarspace10@gmail.com>
2024-05-30 16:13:15 -0600

    Se creo la carpeta para formulario de login y la imagen

---
+++ src/components/FormLogin/index.jsx
@@ -0,0 +1,9 @@
+import react from "react";
+
+const FormLogin = () => {
+    return (
+        <h1>formulario</h1>
+    )
+}
+ 
+export default FormLogin;
---
+++ src/components/ImageButton/index.jsx
@@ -0,0 +1,9 @@
+import react from "react";
+
+const ImageButton = () => {
+    return (
+        <img src='https://media.es.wired.com/photos/6501e7429fa9000811a95fe8/16:9/w_2560%2Cc_limit/Adobe%2520Firefly.jpeg'></img>
+    )
+}
+ 
+export default ImageButton;
